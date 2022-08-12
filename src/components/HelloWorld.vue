a70755dec9743cba8f956cdac875e270e6da5365
yu fujioka <fujiokayu@gmail.com>
2022-08-12 22:54:54 +0900

    add new env for survey

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,42 @@
   <h1>vue xss poc</h1>
   <div class="poc">
     <p class="string">into v-html : <span v-html="inputString"></span></p>
-    <input class="input1" v-model="inputString" placeholder="input your string">
+    <input
+      class="input1"
+      v-model="inputString"
+      placeholder="input your string"
+    />
   </div>
-  <br>
+  <br />
   <div class="poc2">
     <p class="string">into v-bind : {{ userProvidedUrl }}</p>
-    <a v-bind:href="userProvidedUrl"> 
-      Click Me
-    </a>
-    <br>
-    <input class="input2" v-model="userProvidedUrl" placeholder="input your url">
+    <a v-bind:href="userProvidedUrl"> Click Me </a>
+    <br />
+    <input
+      class="input2"
+      v-model="userProvidedUrl"
+      placeholder="input your url"
+    />
   </div>
-  <br>
-  <a href='https://github.com/fujiokayu/vue-xss-poc' target="_blank" rel="noopener">repository</a>
+  <br />
+  <a
+    href="https://github.com/fujiokayu/vue-xss-poc"
+    target="_blank"
+    rel="noopener"
+    >repository</a
+  >
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
+    console.log(process.env.HONEY_CREDENTIALS);
     return {
-      inputString: '',
-      userProvidedUrl: ''
-    }
-  }
-}
+      inputString: "",
+      userProvidedUrl: "",
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
